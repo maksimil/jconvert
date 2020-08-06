82cb892b55995822c336990c81647b497831256f
Maxim <maxim.kosterov@gmail.com>
2020-08-06 21:09:05 +0300

    Generalized window creation

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,18 @@ if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
-const createWindow = () => {
+export const createwindow = (name: string, width: number, height: number) => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: width,
+    height: height,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "index.html"));
+  mainWindow.loadFile(path.join(__dirname, name));
 
   mainWindow.setMenu(null);
 
@@ -32,7 +32,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => createwindow("index.html", 800, 800));
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -47,7 +47,7 @@ app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createwindow("index.html", 800, 800);
   }
 });
 
